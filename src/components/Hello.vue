0e0407768fd0111114ae6cef82c594a85a5fd563
by46 <ycs_ctbu_2010@126.com>
2017-08-26 13:35:07 +0800

    feat: add date range picker component

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,24 +1,36 @@
 <template>
   <div>
     {{msg}}
-    <lte-pagination size="sm"
-                    :totalCount="4003"
-                    @page="onPage"></lte-pagination>
+
+    <div>
+      <lte-pagination size="sm"
+                      :totalCount="4003"
+                      @page="onPage"></lte-pagination>
+    </div>
+    <div>
+      <lte-date-range-picker v-model="dates"></lte-date-range-picker>
+    </div>
   </div>
 </template>
 
 <script>
-  import LtePagination from '@/components/pagination.vue'
+  import LtePagination from '@/components/Pagination.vue'
+  import LteDateRangePicker from '@/components/DateRangePicker.vue'
 
   export default {
     name: 'hello',
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        msg: 'Welcome to Your Vue.js App',
+        dates: {
+          start: new Date(),
+          end: new Date('+14 days')
+        }
       }
     },
     components: {
-      LtePagination
+      LtePagination,
+      LteDateRangePicker
     },
     methods: {
       onPage (currentPage) {
